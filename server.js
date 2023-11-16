f96aaaf3249c5c52be684036c9293e71274c7c14
sdjdd <i@sdjdd.com>
2023-11-16 16:05:52 +0800

    feat: homepage

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  if (req.get('x-fanyi-token') !== token) {
-    return res.status(401).json({ message: 'Forbidden' });
-  }
-  next();
+app.get('/', (req, res) => {
+  res.send('Hello world!');
 });
 
 app.post('/', async (req, res) => {
+  if (req.get('x-fanyi-token') !== token) {
+    return res.status(401).json({ message: 'Forbidden' });
+  }
+
   const { text } = req.body;
   if (typeof text !== 'string') {
     return res.status(400).json({ message: 'text is not a string' });
